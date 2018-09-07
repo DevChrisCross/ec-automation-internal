ac39d16fba7b2f194bbc6803f3649b228444c648
DevChrisCross <devchrismolina@gmail.com>
2018-09-07 08:05:50 +0800

    added support for Optional obj

--- src/test/java/com/elavon/tasks/LoginUser.java
+++ src/test/java/com/elavon/tasks/LoginUser.java
@@ -12,6 +12,8 @@ import net.serenitybdd.screenplay.actions.Enter;
 import net.thucydides.core.annotations.Step;
 import org.openqa.selenium.Keys;
 
+import javax.swing.text.html.Option;
+import java.util.Optional;
 import java.util.ResourceBundle;
 
 public class LoginUser implements Task {
@@ -19,27 +21,38 @@ public class LoginUser implements Task {
     boolean isFromHome;
     private String username;
     private String password;
+    private static final String DEFAULT_CREDENTIAL = "No_Credential_Available";
     private static final ResourceBundle CONFIG = ResourceBundle.getBundle("config");
 
     public LoginUser(UserType user) {
+
         String baseConfigName = "credentials."
                 + CONFIG.getString("environment.type").toLowerCase() + "."
                 + user.toString().toLowerCase();
 
+        isFromHome = false;
+        Optional<String> username = Optional.empty();
+        Optional<String> password = Optional.empty();
+
         try {
-            this.username = CredentialKey.retrieve(baseConfigName + ".username");
-            this.password = CredentialKey.retrieve(baseConfigName + ".password");
+            username = Optional.of(CredentialKey.retrieve(baseConfigName + ".username"));
+            password = Optional.of(CredentialKey.retrieve(baseConfigName + ".password"));
         } catch (Exception e) {
             e.printStackTrace();
+        } finally {
+            this.username = username.orElse(DEFAULT_CREDENTIAL);
+            this.password = password.orElse(DEFAULT_CREDENTIAL);
         }
     }
 
     @Override
     @Step("Login using the user #username")
     public <T extends Actor> void performAs(T actor) {
+
         Performable navigateToLoginPage = isFromHome ?
                 OpenTheApplication.onTheLoginPage().fromTheHomePage() :
                 OpenTheApplication.onTheLoginPage();
+
         actor.attemptsTo(
                 navigateToLoginPage,
                 Enter.theValue(username).into(LoginPage.USERNAME_FIELD),
